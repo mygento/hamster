53530b5837ae1f353b85004a74eaacafea3ceb0b
Nikita Tarasov <maindev@sea-lab.ru>
2015-04-14 18:14:07 +0300

    setup 2

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 set -e
 set -x
 
-git clone https://github.com/mygento/hamster.git "${BUILDENV}"
-chmod +x ${BUILDENV}/install.sh
-${BUILDENV}/install.sh
-cd ${BUILDENV}/public
-${BUILDENV}/bin/phpunit --colors -d display_errors=1
+git clone https://github.com/mygento/hamster.git "${TRAVIS_BUILD_DIR}"
+chmod +x ${TRAVIS_BUILD_DIR}/install.sh
+${TRAVIS_BUILD_DIR}/install.sh
+cd ${TRAVIS_BUILD_DIR}/public
+phpunit --colors -d display_errors=1
