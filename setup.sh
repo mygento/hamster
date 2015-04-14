3d03a9fa7d1726934a05b6885d99923a84e717ed
Nikita Tarasov <maindev@sea-lab.ru>
2015-04-14 18:24:14 +0300

    setup 3

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 set -x
-
-git clone https://github.com/mygento/hamster.git "${TRAVIS_BUILD_DIR}"
-chmod +x ${TRAVIS_BUILD_DIR}/install.sh
-${TRAVIS_BUILD_DIR}/install.sh
-cd ${TRAVIS_BUILD_DIR}/public
+mkdir "${TRAVIS_BUILD_DIR}/magento"
+cd "${TRAVIS_BUILD_DIR}/magento"
+git clone https://github.com/mygento/hamster.git "${TRAVIS_BUILD_DIR}/magento"
+chmod +x ${TRAVIS_BUILD_DIR}/magento/install.sh
+${TRAVIS_BUILD_DIR}/magento/install.sh
+cd ${TRAVIS_BUILD_DIR}/magento/public
 phpunit --colors -d display_errors=1
