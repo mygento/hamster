667e289a2cb1a616f8c8ec25566ddcdb225743ec
Nikita Tarasov <maindev@sea-lab.ru>
2015-04-15 11:48:10 +0300

    setup 7

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ if [ -z $MAGENTO_DB_PORT ]; then MAGENTO_DB_PORT="3306"; fi
 if [ -z $MAGENTO_DB_USER ]; then MAGENTO_DB_USER="root"; fi
 if [ -z $MAGENTO_DB_PASS ]; then MAGENTO_DB_PASS=""; fi
 if [ -z $MAGENTO_DB_NAME ]; then MAGENTO_DB_NAME="magento"; fi
-if [ -z $MAGENTO_DB_ALLOWSAME ]; then MAGENTO_DB_ALLOWSAME="0"; fi
+if [ -z $MAGENTO_DB_ALLOWSAME ]; then MAGENTO_DB_ALLOWSAME="1"; fi
 
 # Get absolute path to main directory
 ABSPATH=$(cd "${0%/*}" 2>/dev/null; echo "${PWD}/${0##*/}")
@@ -31,10 +31,7 @@ sed -i -e s/MAGENTO_DB_HOST/${MAGENTO_DB_HOST}/g ./resources/local.xml.phpunit
 sed -i -e s/MAGENTO_DB_PORT/${MAGENTO_DB_PORT}/g ./resources/local.xml.phpunit
 sed -i -e s/MAGENTO_DB_USER/${MAGENTO_DB_USER}/g ./resources/local.xml.phpunit
 sed -i -e s/MAGENTO_DB_PASS/${MAGENTO_DB_PASS}/g ./resources/local.xml.phpunit
-
-echo "recreating test DB"
-mysql -u${MAGENTO_DB_USER} ${MYSQLPASS} -h${MAGENTO_DB_HOST} -e "DROP DATABASE IF EXISTS ${MAGENTO_DB_NAME}_test; CREATE DATABASE ${MAGENTO_DB_NAME}_test;"
-sed -i -e s/MAGENTO_DB_NAME/${MAGENTO_DB_NAME}_test/g ./resources/local.xml.phpunit
+sed -i -e s/MAGENTO_DB_NAME/${MAGENTO_DB_NAME}/g ./resources/local.xml.phpunit
 echo "installing magento"
 php resources/n98-magerun.phar install \
       --dbHost="${MAGENTO_DB_HOST}" --dbUser="${MAGENTO_DB_USER}" --dbPass="${MAGENTO_DB_PASS}" --dbName="${MAGENTO_DB_NAME}" \
@@ -45,5 +42,8 @@ php resources/n98-magerun.phar install \
       --baseUrl="http://magento.local/" || { echo "Installing Magento failed"; exit 1; }
 echo "installing composer"
 composer self-update
-composer install
+composer install --no-interaction
+cd vendor/ecomdev/ecomdev_phpunit
+git submodule update --init
+cd "${ABSPATH}"
 php resources/n98-magerun.phar --root-dir=public config:set dev/template/allow_symlink 1
